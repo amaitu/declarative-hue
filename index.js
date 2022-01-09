88c595879578a54ea9432ae1b86dfdd90ea56d87
amaitu <steve@sgbarker.com>
2022-01-09 22:07:21 +0000

    lint

--- index.js
+++ index.js
@@ -1,60 +1,70 @@
 const getBrightness = () => {
-    let transitionHour = 20 // Time in the evening when the lights begin to dim.
-    let sunriseHour = 6  // Time in the morning when the lights return to peak brightness.
+  let transitionHour = 20 // Time in the evening when the lights begin to dim.
+  let sunriseHour = 6 // Time in the morning when the lights return to peak brightness.
 
-    const maxSettings = { // % Brightness levels at peak brightness.
-        medium: 100,
-        dim: 100,
-        veryDim: 100,
+  const maxSettings = {
+    // % Brightness levels at peak brightness.
+    medium: 100,
+    dim: 100,
+    veryDim: 100,
+  }
+
+  const minSettings = {
+    // % Brightness levels at dim brightness. Can assign rooms to profiles, e.g. bedroom to veryDim.
+    medium: 60,
+    dim: 40,
+    veryDim: 1,
+  }
+
+  const date = new Date()
+  const currentHours = date.getHours()
+  const currentMinutes = date.getMinutes()
+
+  if (currentHours === transitionHour) {
+    console.log(`it's currently transitionHour (${transitionHour})`)
+    console.log('setting graded dimness')
+    return {
+      medium:
+        maxSettings['medium'] -
+        Math.round((100 - minSettings['medium']) * (currentMinutes / 60)),
+      dim:
+        maxSettings['dim'] -
+        Math.round((100 - minSettings['dim']) * (currentMinutes / 60)),
+      veryDim:
+        maxSettings['veryDim'] -
+        Math.round((100 - minSettings['veryDim']) * (currentMinutes / 60)),
     }
+  }
 
-    const minSettings = { // % Brightness levels at dim brightness. Can assign rooms to profiles, e.g. bedroom to veryDim.
-        medium: 60,
-        dim: 40,
-        veryDim: 1,
-    }
+  if (currentHours > sunriseHour && currentHours < transitionHour) {
+    console.log('setting full brightness')
+    return maxSettings
+  }
 
-    const date = new Date()
-    const currentHours = date.getHours()
-    const currentMinutes = date.getMinutes()
-
-    if (currentHours === transitionHour) {
-        console.log(`it's currently transitionHour (${transitionHour})`)
-        console.log('setting graded dimness')
-        return {
-            medium: maxSettings["medium"] - Math.round((100 - minSettings["medium"]) * (currentMinutes / 60)),
-            dim: maxSettings["dim"] - Math.round((100 - minSettings["dim"]) * (currentMinutes / 60)),
-            veryDim: maxSettings["veryDim"] - Math.round((100 - minSettings["veryDim"]) * (currentMinutes / 60)),
-        }
-    }
-
-    if ((currentHours > sunriseHour) && (currentHours < transitionHour)) {
-        console.log('setting full brightness')
-        return maxSettings
-    }
-
-    console.log('setting peak dimness')
-    return minSettings
-};
+  console.log('setting peak dimness')
+  return minSettings
+}
 
 const handleSchedule = async () => {
-    let brightness = getBrightness()
+  let brightness = getBrightness()
 
-    console.log(`Set brightness levels to medium: ${brightness['medium']}%, dim: ${brightness['dim']}%, veryDim: ${brightness['veryDim']}%`)
-    await fetch(WEBHOOK_URL, {
-        method: "POST",
-        body: JSON.stringify({
-            "value1": brightness['medium'],
-            "value2": brightness['dim'],
-            "value3": brightness['veryDim']
-        }),
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-    })
-};
+  console.log(
+    `Set brightness levels to medium: ${brightness['medium']}%, dim: ${brightness['dim']}%, veryDim: ${brightness['veryDim']}%`,
+  )
+  await fetch(WEBHOOK_URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      value1: brightness['medium'],
+      value2: brightness['dim'],
+      value3: brightness['veryDim'],
+    }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+}
 
-addEventListener("scheduled", event => {
-    event.waitUntil(handleSchedule(event))
+addEventListener('scheduled', event => {
+  event.waitUntil(handleSchedule(event))
 })
